eb28bef775fb36be5315b062d29b0931155f9023
ego-errante <foolt757@gmail.com>
2025-07-30 09:49:12 +0100

    fix zero total cycle

--- crates/broker/src/order_picker.rs
+++ crates/broker/src/order_picker.rs
@@ -625,7 +625,7 @@ where
 
         let proof_res = ProofResult {
             id: "dummy_exec_session_id".to_string(),
-            stats: ExecutorResp { total_cycles: 00_000_000, ..Default::default() },
+            stats: ExecutorResp { total_cycles: 100_000_000, ..Default::default() },
             elapsed_time: 0.0,
         };
 
